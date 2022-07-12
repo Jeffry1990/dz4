5a0439becab7aeb4223f256044bdd57905c916d7
Jeffry1990 <109137757+Jeffry1990@users.noreply.github.com>
2022-07-12 19:27:22 +0300

    Add files via upload

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         int photosCount = 52;
+         int photosRow = 3;
+
+         int fullRowsCount = photosCount / photosRow;
+         int overRowsCount = photosCount % photosRow;
+
+         Console.WriteLine("Полностью заполненных рядов - " + fullRowsCount);
+         Console.WriteLine("Картинок сверх меры - " + overRowsCount);
+        }
+    }
+}
